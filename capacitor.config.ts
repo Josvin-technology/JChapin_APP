392c2b6c3317b4f08b0d17f9950c81425e2c417d
Josvin-technology <100455408+Josvin-technology@users.noreply.github.com>
2026-08-09 20:52:30 -0600

    Agregar carpeta de Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'site.jchapin.app',
   appName: 'j-chapin',
   webDir: 'www'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://nztyidsuzxalgrgrsldy.supabase.co',
+  supabaseKey: 'sb_publishable_NY4sEq3PDSFmBcAz0_QGoA_VK-ujbsz',
 };
