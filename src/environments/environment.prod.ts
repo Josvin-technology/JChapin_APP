6dabb094700bcefa511d9972f48f0eabc2d7fa1b
Josvin-technology <100455408+Josvin-technology@users.noreply.github.com>
2026-08-27 22:59:39 -0600

    Correccion de error de Api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://nztyidsuzxalgrgrsldy.supabase.co',
   supabaseKey: 'sb_publishable_NY4sEq3PDSFmBcAz0_QGoA_VK-ujbsz',
+  googleMapsApiKey: 'AIzaSyC2ARhihAkZOrOasyqMdyPm5BHaVrpJ2fk',
 };
